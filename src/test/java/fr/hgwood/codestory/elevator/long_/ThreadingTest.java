f6c060d466f5fa0c63fc2de4d10ee0b68be73f89
Hugo Wood <hx4.5@free.fr>
2013-12-10 21:07:57 +0100

    ThreadingTest proves the SynchronisingGML eliminates concurrent issues.

---
+++ src/test/java/fr/hgwood/codestory/elevator/long_/ThreadingTest.java
@@ -0,0 +1,38 @@
+package fr.hgwood.codestory.elevator.long_;
+
+import static fr.hgwood.codestory.elevator.Direction.UP;
+
+import java.util.Random;
+
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import fr.hgwood.codestory.elevator.*;
+
+public class ThreadingTest {
+    
+    private final int highestFloor = 10;
+    private final int cabinCount = 10;
+    private final Random random = new Random();
+    private final GameMasterListener sut = 
+        new SynchronizingGameMasterListener(
+        new LoggingGameMasterListener(
+            new ResetableElevatorSystem(), 
+            LoggerFactory.getLogger(ThreadingTest.class)));
+    
+    @BeforeMethod public void setup() {
+        sut.reset(0, highestFloor, 10, cabinCount, "");
+    }
+    
+    @Test(threadPoolSize = 20, invocationCount = 100)
+    public void test() throws Exception {
+        sut.call(random.nextInt(highestFloor), UP);
+        sut.go(random.nextInt(cabinCount), random.nextInt(highestFloor));
+        int n = random.nextInt(cabinCount);
+        sut.userHasEntered(n);
+        sut.userHasExited(n);
+        sut.nextCommands();
+    }
+
+}
